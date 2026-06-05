3917289b5e951bb7ec872ae046f7d884226a2e3e
Tanay09876 <152384332+Tanay09876@users.noreply.github.com>
2026-06-05 21:41:30 +0530

    Implement cached Mongoose connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,37 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-    try {
-        mongoose.connection.on('connected', ()=> console.log("Database connected"))
-        await mongoose.connect(process.env.MONGODB_URI)
-    } catch (error) {
-        console.error("Database connection failed:", error.message)
-    }
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
+
+const connectDB = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    mongoose.connection.on('connected', () => console.log("Database connected"));
+
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, opts).then((m) => {
+      return m;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    console.error("Database connection failed:", error.message);
+    throw error;
+  }
+
+  return cached.conn;
+};
 
 export default connectDB;
